4c23e972cc882edbbffd3cfcced46958a15a12f7
unknown <tangchif@gmail.com>
2020-07-21 12:02:52 +0100

    caught error on unspecified id

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import About from "./components/About";
 import Photography from "./components/Photography";
 import Home from "./components/Home";
 import PhotoPage from "./components/photography/PhotoPage";
+import NotFound from "./components/NotFound";
+
+import AlbumErrorBoundary from "./components/photography/ErrorBoundary";
 
 function App() {
   return (
@@ -17,7 +20,12 @@ function App() {
           <Route path="/" exact component={Home} />
           <Route path="/about" component={About} />
           <Route path="/photography" exact component={Photography} />
-          <Route path="/photography/:id" exact component={PhotoPage} />
+          <Route path="/photography/:id" exact>
+            <AlbumErrorBoundary>
+              <PhotoPage />
+            </AlbumErrorBoundary>
+          </Route>
+          <Route path="*" component={NotFound} />
         </Switch>
       </Router>
     </>
---
+++ src/components/NotFound.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function NotFound() {
+  return (
+    <div className="not-found">
+      <h1>404 page not found</h1>
+      <p>please go back.</p>
+    </div>
+  );
+}
+
+export default NotFound;
