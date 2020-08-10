12f74d2f76d5b9b553597c53f0c04455d542a3d7
unknown <tangchif@gmail.com>
2020-08-10 18:38:43 +0100

    scroll to top

--- src/components/Burger.js
+++ src/components/Burger.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 // import { NavLink } from "react-router-dom";
 import { NavHashLink as NavLink } from "react-router-hash-link";
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 
 function Burger() {
   const [clicked, setClicked] = useState({ status: false });
@@ -23,6 +25,8 @@ function Burger() {
       <div className="menu-container">
         <div className="menu-items">
           <ul>
+            <ScrollToTop />
+
             <NavLink exact to="/" onClick={handleMenu}>
               <li className="underline-hover">Home</li>
             </NavLink>
@@ -32,10 +36,10 @@ function Burger() {
             <NavLink to="/#work" onClick={handleMenu}>
               <li className="underline-hover">Work</li>
             </NavLink>
-
             <NavLink to="/photography" onClick={handleMenu}>
               <li className="underline-hover">Photo</li>
             </NavLink>
+            {/* </ScrollToTop> */}
             <NavLink to="/#contact" onClick={handleMenu}>
               <li className="underline-hover">Contact</li>
             </NavLink>
@@ -44,5 +48,14 @@ function Burger() {
       </div>
     </>
   );
+}
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
 }
 export default Burger;
