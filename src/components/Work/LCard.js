8d5f5311d203aed99bf9cefbfca4ecaad8d9644a
unknown <tangchif@gmail.com>
2020-08-12 18:52:35 +0100

    controls

--- src/components/Work/LCard.js
+++ src/components/Work/LCard.js
@@ -6,7 +6,7 @@ import {
   useFrame,
   useLoader,
 } from "react-three-fiber";
-// import { useSpring, a } from "react-spring/three";
+import { useSpring, a } from "react-spring/three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 // import card from '../../content/card2.gltf'
@@ -52,27 +52,37 @@ extend({ OrbitControls });
 //     // null
 //   );
 // }
-function Card(props) {
+
+function Card() {
   const gltf = useLoader(GLTFLoader, "/card56.gltf");
   const primitive = useRef();
-
+  let num = 0.0;
   const [active, setActive] = useState(false);
+  const [hovered, setHovered] = useState(false);
+  const props = useSpring({
+    scale: active ? [1.5, 1.5, 1.5] : [1, 1, 1],
+    spin: hovered ? (num = 0.0) : (num = 0.01),
+  });
 
   useFrame(
-    () => (primitive.current.rotation.x = primitive.current.rotation.y += 0.005)
+    () => (primitive.current.rotation.x = primitive.current.rotation.y += num)
   );
 
   return (
-    <primitive
-      {...props}
-      ref={primitive}
-      object={gltf.scene}
-      material={gltf.scene}
-      scale={active ? [1.5, 1.5, 1.5] : [1, 1, 1]}
-      onClick={(e) => setActive(!active)}
-      position={[0, 0, 0]}
-      rotation={[-Math.PI / 2, 0, 0]}
-    />
+    <group>
+      <a.primitive
+        ref={primitive}
+        object={gltf.scene}
+        material={gltf.scene}
+        scale={props.scale}
+        // scale={console.log(props.scale)}
+        onClick={() => setActive(!active)}
+        onPointerOver={() => setHovered(true)}
+        onPointerOut={() => setHovered(false)}
+        position={[0, 0, 0]}
+        rotation={[Math.PI / 2, Math.PI / 1, 0]}
+      />
+    </group>
   );
 }
 
