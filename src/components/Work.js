606bb1765f086386ab662178282556a0a14ff0c4
unknown <tangchif@gmail.com>
2020-08-13 16:32:17 +0100

    description

--- src/components/Work.js
+++ src/components/Work.js
@@ -9,13 +9,30 @@ function MyWork() {
         <p>tetris</p>
         <p>Sun Dragon Loyalty Cards</p> */}
         <div className="work-grid">
-          <div className="item">
-            {/* <div className="info">Sun Dragon Loyalty Cards</div> */}
-
+          <div className="item 1">
+            <h2 className="info">Sun Dragon Loyalty Cards</h2>
             <LCard />
           </div>
-          <div className="item">2</div>
-          <div className="item">3</div>
+          <div className="desc">
+            <p>
+              My parent's restaurant needed loyalty cards for a new takeaway
+              item which quickly gained popularity. I created the design using
+              InDesign and got them printed by a local printing company. The
+              cards were succesful in keeping customer retention.
+            </p>
+          </div>
+          <div className="item">
+            <h2>Tetris Clone</h2>
+            <a href="https://keen-bhaskara-31058f.netlify.app/">
+              <img
+                src="https://res.cloudinary.com/drfbjbnqj/image/upload/v1597260402/blockscreenshot_pjzz60.png"
+                alt="blocks"
+              ></img>
+            </a>
+            <p>Teaching myself the fundamentals of javascript</p>
+
+            <div></div>
+          </div>
         </div>
       </div>
     </>
